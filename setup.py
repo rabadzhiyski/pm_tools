02d2ff139af19bd0726eecc09e552db4c9ec57ec
rabadzhiyski <plamen.rabadzhiyski@gmail.com>
2021-01-21 15:38:49 +0200

    creating a setup.py file for the package

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Setup file for package pm_tools
+
+from setuptools import setup
+
+setup(name='pm_tools',
+      version='0.1',
+      description='Project Management Metrics',
+      packages=['pm_tools'],
+      author = "Plamen Rabadzhiyski",
+      author_email = "plamen@pmtonomy.com",
+      zip_safe=False)
