c30f37a2666a5e78f88af24baa241b6ad680f87f
rabadzhiyski <plamen.rabadzhiyski@gmail.com>
2021-01-21 18:46:31 +0200

    initiated the python scrypt

---
+++ pm_tools/pm_tools.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+This is a script file for calculating the most
+popular project management metrics.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Add general Class
+
+# planned_value = planned % complete * budget
+
+# earned_value = actual % complete * budget
+
+# schedule_variance = earned_value - planned_value
+
+# cost_variance = earned_value - actual cost
+
+# schedule_perf_index = earned_value / planned_value
+
+# cost_perf_index = earned_value / actual cost
+
+# create one example object per milestone
+
+# calculate the respective function
